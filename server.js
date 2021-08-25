85ea74e7027cee22dae4482604d649387eb2cca2
neerajmasai <neeraj_fw10_110@masai.school>
2021-08-25 13:25:15 +0530

    added backend architecture for controller,models

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const express = require("express");
 const app = express();
 app.listen(PORT, () => console.log(`Nordstrom Web App connected successfully to Express. Listening on port ${PORT}...`));
 
+//middlewares
+app.use(express.json());
+
 //connect to mongoose
 const mongoose = require('mongoose');
 mongoose.connect(DEFAULT_CONNECTION_STRING, MONGOOSE_OPTIONS);
 mongoose.connection.on("error", err => {
-  console.log("Connection Error. Nordstrom Web App could not successfully to Mongoose.", err);
+  console.log("Connection Error. Nordstrom Web App could not successfully connect to Mongoose.", err);
 });
 mongoose.connection.on("connected", (err, res) => {
     console.log("Nordstrom Web App connected successfully to Mongoose.");
 });
+
+//routes
+
