0d1e5c060e57ff52d6d7c2a9c0ffa0175a487334
Rabia-Basari777 <rabia_fw10_200@masai.school>
2021-08-27 17:24:13 +0530

    redirection to different pages added

--- paymentsMethod_page/payments_page.js
+++ paymentsMethod_page/payments_page.js
@@ -4,3 +4,18 @@ homeRedirect.addEventListener("click", redirectToHome);
 function redirectToHome() {
     window.location.href = "../landing_page/landingPage.html";
 }
+
+let shippingDiv = document.getElementById("shippingDiv");
+shippingDiv.addEventListener("click", function () {
+    window.location.href = "../shippingAddress_page/shipping_page.html";
+})
+
+let personalDiv = document.getElementById("personalDiv");
+personalDiv.addEventListener("click", function () {
+    window.location.href = "../personalInfo_page/personalInfo_page.html";
+})
+
+let purchaseDiv = document.getElementById("purchaseDiv");
+purchaseDiv.addEventListener("click", function () {
+    window.location.href = "../purchase_page/purchase_page.html";
+})
--- shippingAddress_page/shipping_page.js
+++ shippingAddress_page/shipping_page.js
@@ -4,3 +4,19 @@ homeRedirect.addEventListener("click", redirectToHome);
 function redirectToHome() {
     window.location.href = "../landing_page/landingPage.html";
 }
+
+
+let purchaseDiv = document.getElementById("purchaseDiv");
+purchaseDiv.addEventListener("click", function () {
+    window.location.href = "../purchase_page/purchase_page.html";
+})
+
+let personalDiv = document.getElementById("personalDiv");
+personalDiv.addEventListener("click", function () {
+    window.location.href = "../personalInfo_page/personalInfo_page.html";
+})
+
+let paymentDiv = document.getElementById("paymentDiv");
+paymentDiv.addEventListener("click", function () {
+    window.location.href = "../paymentsMethod_page/payments_page.html";
+})
