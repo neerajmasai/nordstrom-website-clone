d15c0685c6136f570185d08f8a14674b6d2af7b7
neerajmasai <neeraj_fw10_110@masai.school>
2021-08-29 14:04:29 +0530

    account info and initials functionality

--- personalInfo_page/personalInfo_page.js
+++ personalInfo_page/personalInfo_page.js
@@ -20,3 +20,28 @@ let paymentDiv = document.getElementById("paymentDiv");
 paymentDiv.addEventListener("click", function () {
     window.location.href = "../paymentsMethod_page/payments_page.html";
 })
+
+function loadAccountInfo(){
+    /* loads account name and initials */
+
+  //get user
+  const currentEmail = JSON.parse(localStorage.getItem("currentEmail"));
+  console.log(currentEmail);
+  fetch(`http://localhost:2345/users/query/${currentEmail}`)
+  .then(response => response.json())
+  .then((userData) => {
+      
+      //first name initial
+      document.getElementById("accountInitial").innerHTML = userData[0]["firstName"][0];
+
+      //first name greeting
+      document.getElementById("accountName").innerHTML = `${userData[0]["firstName"]}'s Account`;
+
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
+    
+}
+
+loadAccountInfo();
--- purchase_page/purchase_page.js
+++ purchase_page/purchase_page.js
@@ -111,4 +111,29 @@ function appendPurchaseOrders(orders){
     });
 
 }
+
+function loadAccountInfo(){
+    /* loads account name and initials */
+
+  //get user
+  const currentEmail = JSON.parse(localStorage.getItem("currentEmail"));
+  console.log(currentEmail);
+  fetch(`http://localhost:2345/users/query/${currentEmail}`)
+  .then(response => response.json())
+  .then((userData) => {
+      
+      //first name initial
+      document.getElementById("accountInitial").innerHTML = userData[0]["firstName"][0];
+
+      //first name greeting
+      document.getElementById("accountName").innerHTML = `${userData[0]["firstName"]}'s Account`;
+
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
+    
+}
+
+loadAccountInfo();
 loadPurchases();
