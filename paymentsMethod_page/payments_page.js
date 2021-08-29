a649043c242c8d06e7c4444f12406580d19f51cb
Rabia-Basari777 <rabia_fw10_200@masai.school>
2021-08-29 12:37:28 +0530

    payments page dynamic

--- paymentsMethod_page/payments_page.js
+++ paymentsMethod_page/payments_page.js
@@ -19,3 +19,47 @@ let purchaseDiv = document.getElementById("purchaseDiv");
 purchaseDiv.addEventListener("click", function () {
     window.location.href = "../purchase_page/purchase_page.html";
 })
+
+// loading the card details dynamically
+
+// function loadPaymentMethod() {
+//     /* load transactions from database for current user */
+
+//     //get user id
+//     const currentEmail = JSON.parse(localStorage.getItem("currentEmail"));
+//     console.log(currentEmail);
+//     fetch(`http://localhost:2345/users/query/${currentEmail}`)
+//     .then(response => response.json())
+//     .then((data) => {
+//         const userId = data[0]["_id"];
+
+//         //get transactions for user
+//         fetch(`http://localhost:2345/users/${userId}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             //update front end
+//             // appendPurchaseOrders(data);
+//             appendPaymentDetails(data)
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         });
+
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+// }
+
+// // appending user card details
+
+// function appendPaymentDetails(details) {
+//     const nameOnCard = document.getElementById("nameOnCard").value = details.paymentMethod.nameOnCard;
+//     const cardNumber = document.getElementById("cardNumber").value = details.paymentMethod.cardNumber;
+//     const expMonth = document.getElementById("expMonth").value = details.paymentMethod.expMonth;
+//     const expYear = document.getElementById("expYear").value = details.paymentMethod.expYear;
+//     const cvv = document.getElementById("cvv").value = details.paymentMethod.cvv;
+// }
+
+// loadPaymentMethod()
